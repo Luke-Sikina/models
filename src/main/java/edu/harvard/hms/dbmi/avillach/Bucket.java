6b756e183ecdba84f0d3d283bd9b71380e7d6e53
Luke Sikina <lucas.sikina@gmail.com>
2023-03-27 09:49:27 -0400

    models

---
+++ src/main/java/edu/harvard/hms/dbmi/avillach/Bucket.java
@@ -0,0 +1,3 @@
+package edu.harvard.hms.dbmi.avillach;
+
+public record Bucket(String name, String ownerID) { }
---
+++ src/main/java/edu/harvard/hms/dbmi/avillach/User.java
@@ -0,0 +1,3 @@
+package edu.harvard.hms.dbmi.avillach;
+
+public record User(String awsID, String email) {}
